5ca355740edd53da4bef61fd55b2c1b80e3ec37e
Ethan White <ethan@weecology.org>
2025-03-11 20:35:16 -0400

    Complete draft of data creation code

--- prepare-data.R
+++ prepare-data.R
@@ -5,17 +5,19 @@ library(tidyr)
 
 #' Create distributed lag matrices for environmental covariates
 #' Code modified from Clark et al. 2024
-#' df should have a time column, a series column, and a value column
+#' df should have a newmoonnumber column and a value column
 #' the value column will be lagged
 lagard <- function(df, n_lag = 6) {
+  covar <- names(df)[2]
   n <- nrow(df)
-  time_series <- pull(select(df, -time, -series)) # Get just values
+  time_series <- covars[[2]] # Get just values
   lags <- matrix(NA, n, n_lag)
   for (i in 1:n_lag) {
     lags[i:n, i] <- time_series[i:n - i + 1]
   }
   lags_df <- data.frame(lags)
-  bind_cols(select(df, time, series), lags_df)
+  colnames(lags_df) <- paste0(covar, "_lag_", 0:(n_lag - 1))
+  bind_cols(df["newmoonnumber"], lags_df)
 }
 
 download_observations()
@@ -88,7 +90,8 @@ covars <- weather |>
     )),
     mintemp = scale(mintemp),
     maxtemp = scale(maxtemp),
-    meantemp_lag1 = scale(lag(meantemp, order_by = newmoonnumber)), # From Pat's paper
+    # From Pat's paper
+    meantemp_lag_1 = scale(lag(meantemp, order_by = newmoonnumber)),
   ) |>
   select(
     newmoonnumber,
@@ -100,35 +103,77 @@ covars <- weather |>
     cool_precip,
     ndvi,
     ndvi_ma12,
-    meantemp_lag1,
+    meantemp_lag_1,
     mintemp_ma3,
     maxtemp_ma3
   )
 
-# TODO: See if we need to keep the month and year columns created here
-#       Think we don't need them but will keep for the moment
+# Temperature 6-month lag matrices
+mintemp_df <- lagard(select(covars, newmoonnumber, mintemp), 6)
+maxtemp_df <- lagard(select(covars, newmoonnumber, maxtemp), 6)
+
+covars_w_lags <- covars |>
+  inner_join(mintemp_df, by = c("newmoonnumber")) |>
+  inner_join(maxtemp_df, by = c("newmoonnumber"))
+
 model_dat <- rodent_data |>
-  right_join(covars, by = "newmoonnumber") |>
-  # add mvgam columns + month and year
+  right_join(covars_w_lags, by = "newmoonnumber") |>
+  # add mvgam columns
   mutate(
     y = abundance,
     series = species,
-    time = newmoonnumber - (min(newmoonnumber) - 1),
-    month = lubridate::month(censusdate),
-    year = lubridate::year(censusdate)
+    time = newmoonnumber - (min(newmoonnumber) - 1)
   ) |>
-  select(time, y, series, month, year, newmoonnumber, mintemp:maxtemp_ma3) |>
-  drop_na(mintemp:maxtemp_ma3) |>
+  select(time, y, series, newmoonnumber, meantemp:maxtemp_lag_5) |>
+  drop_na(meantemp:maxtemp_lag_5) |>
   arrange(time, series)
 
 # Compared to Clarke et al. 2025 we don't need to filter species
 # by occurance because we already filtered by name
 
-# Mintemp 6-month lag matrix
-mintemp_df <- lagard(select(model_dat, time, series, mintemp), 6)
-maxtemp_df <- lagard(select(model_dat, time, series, maxtemp), 6)
+lag <- matrix(0:5, nrow(model_dat), 6, byrow = TRUE)
 
-lag <- matrix(0:5, nrow(model_dat),
-  6,
-  byrow = TRUE
+# Create weight matrices for hierarchical distributed lag terms
+# Directly from Clarke et al. 2025's code
+weights_dm <- weights_do <-
+  weights_pp <- weights_ol <-
+  weights_ot <- weights_pf <-
+  weights_pb <- weights_pe <- weights_rm <-
+  matrix(1, ncol = ncol(lag), nrow = nrow(lag))
+
+weights_dm[!(model_dat$series == "DM"), ] <- 0
+weights_do[!(model_dat$series == "DO"), ] <- 0
+weights_ol[!(model_dat$series == "OL"), ] <- 0
+weights_ot[!(model_dat$series == "OT"), ] <- 0
+weights_pb[!(model_dat$series == "PB"), ] <- 0
+weights_pe[!(model_dat$series == "PE"), ] <- 0
+weights_pf[!(model_dat$series == "PF"), ] <- 0
+weights_pp[!(model_dat$series == "PP"), ] <- 0
+weights_rm[!(model_dat$series == "RM"), ] <- 0
+
+data_all <- list(
+  lag = lag,
+  meantemp = model_dat$meantemp,
+  meantemp_lag_1 = model_dat$meantemp_lag_1,
+  mintemp = as.matrix(select(model_dat, mintemp_lag_1:mintemp_lag_5)),
+  mintemp_ma3 = model_dat$mintemp_ma3,
+  maxtemp = as.matrix(select(model_dat, maxtemp_lag_1:maxtemp_lag_5)),
+  maxtemp_ma3 = model_dat$maxtemp_ma3,
+  warm_precip = model_dat$warm_precip,
+  cool_precip = model_dat$cool_precip,
+  ndvi_ma12 = model_dat$ndvi_ma12,
+  weights_dm = weights_dm,
+  weights_do = weights_do,
+  weights_ol = weights_ol,
+  weights_ot = weights_ot,
+  weights_pb = weights_pb,
+  weights_pe = weights_pe,
+  weights_pf = weights_pf,
+  weights_pp = weights_pp,
+  weights_rm = weights_rm,
+  y = model_dat$y,
+  series = model_dat$series,
+  time = model_dat$time
 )
+
+saveRDS(data_all, file = "data_all.rds")
