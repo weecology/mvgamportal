c79b6d4528bb03d2dd7372109a87cc788cdc8aa8
Ethan White <ethan@weecology.org>
2026-08-11 08:43:57 -0400

    Add single species seasonal AR and temperature AR models
    The seasonal model requires adding month data to the data files hence
    the modifications to the data and data generation code.

---
+++ R/models/sAR_separate.R
@@ -0,0 +1,16 @@
+fit_sAR_separate <- function(data_train, data_test, species_list) {
+  mvgam(
+    formula = y ~ 1,
+    trend_formula = ~ s(censusmonth, k = 12, bs = "cc"),
+    trend_knots = list(censusmonth = seq(0.5, 12.5, length.out = 12)),
+    data = data_train,
+    newdata = data_test,
+    family = nb(),
+    trend_model = AR(p = 1),
+    burnin = 5000,
+    samples = 2000,
+    silent = 2,
+    refresh = 0,
+    control = list(adapt_delta = 0.95) # Increase from default 0.8 to decrease divergences
+  )
+}
