4fabd3328e9570fd904dfd6f2255d8bcb8ad5fa6
Ethan White <ethan@weecology.org>
2026-08-13 09:51:28 -0400

    Update HPC script with better naming and more CPUs

--- sliding_window_slurm.sh
+++ sliding_window_slurm.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 # Job name and who to send updates to
-#SBATCH --job-name=portal_sliding_window
+#SBATCH --job-name=4casttime
 #SBATCH --mail-user=ethanwhite@ufl.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH --account=ewhite
 #SBATCH --qos=ewhite
 
 # Where to put the outputs: %j expands into the job number (a unique identifier for this job)
-#SBATCH --output my_job%j.out
-#SBATCH --error my_job%j.err
+#SBATCH --output 4casttime%j.out
+#SBATCH --error 4casttime%j.err
 
 # Number of nodes to use
 #SBATCH --nodes=1
 
 # Number of tasks (usually translate to processor cores) to use: important! this means the number of mpi ranks used, useless if you are not using Rmpi)
-#SBATCH --ntasks=1
+#SBATCH --ntasks=26
 
-#number of cores to parallelize with:
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=16000
+# Memory
+#SBATCH --mem=4gb
 
-# Job run time in [DAYS]
-# HOURS:MINUTES:SECONDS
+# Job run time in
+# [DAYS]:HOURS:MINUTES:SECONDS
 # [DAYS] are optional, use when it is convenient
-#SBATCH --time=96:00:00
+#SBATCH --time=24:00:00
 
 # Save some useful information to the "output" file
 date;hostname;pwd
